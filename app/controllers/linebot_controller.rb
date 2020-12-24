47cea37fa3355943e8a67bff425170de58da6f91
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-12-24 15:27:31 +0900

    sticker_respond

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -156,7 +156,8 @@ class LinebotController < ApplicationController
   def handle_sticker(event)
     # Message API available stickers
     # https://developers.line.me/media/messaging-api/sticker_list.pdf
-    msgapi_available = event.message['packageId'].to_i <= 4
+    AvailableStickerList = [11537, 11538, 11539]
+    msgapi_available = AvailableStickerList.include? event.message['packageId']
     messages = [{
       type: 'text',
       text: "[STICKER]\npackageId: #{event.message['packageId']}\nstickerId: #{event.message['stickerId']}"
@@ -165,7 +166,7 @@ class LinebotController < ApplicationController
       messages.push(
         type: 'sticker',
         packageId: event.message['packageId'],
-        stickerId: event.message['stickerId']
+        stickerId: event.message['stickerId']+1
       )
     end
     reply_content(event, messages)

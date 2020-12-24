c0676219b3b3978a17a4b7be313917f996bab2da
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-12-24 15:59:02 +0900

    random_sticker

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -156,19 +156,15 @@ class LinebotController < ApplicationController
   def handle_sticker(event)
     # Message API available stickers
     # https://developers.line.me/media/messaging-api/sticker_list.pdf
-    availableStickerList = [11537, 11538, 11539]
-    msgapi_available = availableStickerList.include? event.message['packageId'].to_i
+
+    package_id = event.message['packageId'].to_i
+    p_id, s_id = get_random_sticker(package_id)
     messages = [{
-      type: 'text',
-      text: "[STICKER]\npackageId: #{event.message['packageId']}\nstickerId: #{event.message['stickerId']}"
-    }]
-    if msgapi_available
-      messages = [{
         type: 'sticker',
-        packageId: event.message['packageId'],
-        stickerId: event.message['stickerId']
+        packageId: p_id,
+        stickerId: s_id
       }]
-    end
+
     reply_content(event, messages)
   end
 
@@ -186,5 +182,31 @@ class LinebotController < ApplicationController
       type: 'text',
       text: "[UNSEND]\nmessageId: #{event['unsend']['messageId']}"
     })
+  end
+
+  def get_random_sticker(id)
+    availableStickerList = [
+      ['11537',
+        %[52002734 52002735 52002736 52002737 52002738 52002739 52002740 52002741 52002742 52002743
+        52002744 52002745 52002746 52002747 52002748 52002749 52002750 52002751 52002752 52002753
+        52002754 52002755 52002756 52002757 52002758 52002759 52002760 52002761 52002762 52002763
+        52002764 52002765 52002766 52002767 52002768 52002769 52002770 52002771 52002772 52002773].split
+      ],
+      ['11538',
+        %[51626494 51626495 51626496 51626497 51626498 51626499 51626500 51626501 51626502 51626503
+          51626504 51626505 51626506 51626507 51626508 51626509 51626510 51626511 51626512 51626513
+          51626514 51626515 51626516 51626517 51626518 51626519 51626520 51626521 51626522 51626523
+          51626524 51626525 51626526 51626527 51626528 51626529 51626530 51626531 51626532 51626533].split
+      ],
+      ['11539',
+        %[52114110 52114111 52114112 52114113 52114114 52114115 52114116 52114117 52114118 52114119
+          52114120 52114121 52114122 52114123 52114124 52114125 52114126 52114127 52114128 52114129
+          52114130 52114131 52114132 52114133 52114134 52114135 52114136 52114137 52114138 52114139
+          52114140 52114141 52114142 52114143 52114144 52114145 52114146 52114147 52114148 52114149].split
+      ]
+    ]
+    id = [0,1,2].sample if id.nil?
+    id ||= (id.to_i+1) % 3
+    [availableStickerList[id], availableStickerList[id][1].sample]
   end
 end

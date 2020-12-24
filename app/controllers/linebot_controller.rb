17c4e1164d0b1894641026a0f0cb1fa97e92c8f3
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-12-24 15:18:12 +0900

    sticker

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -47,6 +47,8 @@ class LinebotController < ApplicationController
 
   def handle_message(event)
     case event.type
+    when Line::Bot::Event::MessageType::Sticker
+      handle_sticker(event)
     when Line::Bot::Event::MessageType::Text
       message = event.message['text']
 
@@ -149,6 +151,24 @@ class LinebotController < ApplicationController
     )
     logger.warn res.read_body unless Net::HTTPOK === res
     res
+  end
+
+  def handle_sticker(event)
+    # Message API available stickers
+    # https://developers.line.me/media/messaging-api/sticker_list.pdf
+    msgapi_available = event.message['packageId'].to_i <= 4
+    messages = [{
+      type: 'text',
+      text: "[STICKER]\npackageId: #{event.message['packageId']}\nstickerId: #{event.message['stickerId']}"
+    }]
+    if msgapi_available
+      messages.push(
+        type: 'sticker',
+        packageId: event.message['packageId'],
+        stickerId: event.message['stickerId']
+      )
+    end
+    reply_content(event, messages)
   end
 
   def handle_unsend(event)

782b0b1ce81f5c044027f40a240d2ee70d98b48d
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-12-25 14:18:02 +0900

    home_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'liff#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'linebot#callback'
 end
